b8bc8370bd265f9f94c7c9227fde5356674446d9
trumpet-pok <47871563+trumpet-pok@users.noreply.github.com>
2019-02-21 23:41:19 +0100

    Add files via upload

---
+++ pred.app/pred.app/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using pred.app.Models;
+
+namespace pred.app.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult About()
+		{
+			ViewData["Message"] = "Something about me.";
+
+			return View();
+		}
+
+		public IActionResult Contact()
+		{	
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ pred.app/pred.app/TagHelpers/VersionTagHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace pred.app.TagHelpers
+{
+	using System.Reflection;
+	using Microsoft.AspNetCore.Razor.TagHelpers;
+	// You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+	[HtmlTargetElement("version")]
+	public class VersionTagHelper : TagHelper
+	{
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = "span";
+			output.Content.SetContent($"(version {this.GetAppVersion()})");
+			output.TagMode = TagMode.StartTagAndEndTag;
+		}
+		private string GetAppVersion()
+		{
+			string assemblyVersion = Assembly.GetEntryAssembly()
+				.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+			return assemblyVersion;
+		}
+	}
+}
